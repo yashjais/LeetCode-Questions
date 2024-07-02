5b50f8fdb459455c0b647a4d54eba126c5699540
Yash Jaiswal <51019768+yashjais@users.noreply.github.com>
2024-07-02 08:08:46 +0000

    Added some more code in Intersection of Two Arrays II question

--- Questions/Array/intersection-of-two-arrays-2.js
+++ Questions/Array/intersection-of-two-arrays-2.js
@@ -26,30 +26,116 @@
  * @param {number[]} nums2
  * @return {number[]}
  */
-var intersect = function (nums1, nums2) {
-  const map1 = new Map();
-  const resultArr = [];
+// /**
+//  * @param {number[]} nums1
+//  * @param {number[]} nums2
+//  * @return {number[]}
+//  */
+// var intersect = function(nums1, nums2) {
+//     const map1 = new Map();
+//     const resultArr = [];
 
-  const nums1Len = nums1.length;
-  for (let i = 0; i < nums1Len; i += 1) {
-    if (map1.has(nums1[i])) {
-      const count = map1.get(nums1[i]);
-      map1.set(nums1[i], count + 1);
-    } else {
-      map1.set(nums1[i], 1);
+//     const nums1Len = nums1.length;
+//     for (let i = 0; i < nums1Len; i += 1) {
+//         if (map1.has(nums1[i])) {
+//             const count = map1.get(nums1[i]);
+//             map1.set(nums1[i], (count + 1));
+//         } else {
+//             map1.set(nums1[i], 1)
+//         }
+//     }
+
+//     const nums2Len = nums2.length;    
+//     for (let i = 0; i < nums2Len; i += 1) {
+//         if (map1.get(nums2[i])) {
+//             resultArr.push(nums2[i])
+//             const count = map1.get(nums2[i]);
+//             map1.set(nums2[i], count - 1)
+//         }
+//     }
+
+//     return resultArr;
+// };
+
+// solving as a daily challenge on 2nd July
+// var intersect = function(nums1, nums2) {
+//     const numMap = new Map();
+//     const nums1Len = nums1.length;
+//     const nums2Len = nums2.length;
+//     const resultArr = [];
+
+//     for (let i = 0; i < nums1Len; i += 1) {
+//         if (numMap.has(nums1[i])) {
+//             numMap.set(nums1[i], numMap.get(nums1[i]) + 1);
+//         } else {
+//             numMap.set(nums1[i], 1);
+//         }
+//     }
+
+//     for (let i = 0; i < nums2Len; i += 1) {
+//         if (numMap.has(nums2[i])) {
+//             const count = numMap.get(nums2[i]);
+//             if (count === 0) {
+//                 numMap.delete(nums2[i]);
+//             } else {
+//                 resultArr.push(nums2[i]);
+//                 numMap.set(nums2[i], count - 1);
+//             }
+//         }
+//     }
+
+//     return resultArr;
+// }
+
+const countingSort = (nums) => {
+    const numsLength = nums.length;
+    const countArray = new Array(1001).fill(0);
+
+    for (let i = 0; i < numsLength; i += 1) {
+        countArray[nums[i]] += 1;
     }
-  }
 
-  const nums2Len = nums2.length;
-  for (let i = 0; i < nums2Len; i += 1) {
-    if (map1.get(nums2[i])) {
-      resultArr.push(nums2[i]);
-      const count = map1.get(nums2[i]);
-      map1.set(nums2[i], count - 1);
+    for (let i = 1; i < 1002; i += 1) {
+        countArray[i] += countArray[i - 1];
     }
-  }
 
-  return resultArr;
+
+    const resultArr = [];
+    for (let i = 1001; i >= 0; i -= 1) {
+        resultArr[countArray[nums[i]] - 1] = nums[i];
+        countArray[nums[i]] -= 1;
+    }
+
+    return resultArr;
+};
+
+var intersect = function(nums1, nums2) {
+    nums1 = nums1.sort((a, b) => a - b);
+    nums2 = nums2.sort((a, b) => a - b);
+    const nums1Len = nums1.length;
+    const nums2Len = nums2.length;
+    // nums1 = countingSort(nums1);
+    // nums2 = countingSort(nums2);
+
+    let i = 0;
+    let j = 0;
+    let returnArray = [];
+
+    while (j < nums2Len && i < nums1Len){
+        if (nums1[i] === nums2[j]){
+            returnArray.push(nums1[i]);
+            i += 1;
+            j += 1;
+        }
+        else if (nums1[i] > nums2[j]){
+            j += 1;
+        }
+        else {
+            i += 1;
+        }
+    }
+
+    return returnArray;
 };
 
 console.log(intersect((nums1 = [1, 2, 2, 1]), (nums2 = [2, 2])));
